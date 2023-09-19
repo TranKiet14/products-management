59aae09d177d5303fcc6bc368c498a5e48204023
Trần Anh Kiệt <trananhkiet14032002@gmail.com>
2023-09-19 14:25:42 +0700

    products-management

---
+++ public/admin/js/script.js
@@ -0,0 +1,144 @@
+//button status
+const buttonsStatus = document.querySelectorAll("[button-status]");
+if (buttonsStatus.length > 0) {
+  let url = new URL(window.location.href);
+  buttonsStatus.forEach(button => {
+    button.addEventListener("click", () => {
+      const status = button.getAttribute("button-status");
+
+      if (status) {
+        url.searchParams.set("status", status);
+      } else {
+        url.searchParams.delete("status");
+      }
+      
+      window.location.href = url.href;
+    });
+  });
+}
+//end button-status
+//Form search
+const formSearch = document.querySelector("#form-search");
+if (formSearch) {
+  let url = new URL(window.location.href);
+  formSearch.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const keyword = e.target.elements.keyword.value;
+    if (keyword) {
+      url.searchParams.set("keyword", keyword);
+    } else {
+      url.searchParams.delete("keyword");
+    }
+    window.location.href = url.href;
+  })
+}
+
+// Pagination
+const buttonsPagination = document.querySelectorAll("[button-pagination]");
+if(buttonsPagination) {
+  let url = new URL(window.location.href);
+
+  buttonsPagination.forEach(button => {
+    button.addEventListener("click", () => {
+      const page = button.getAttribute("button-pagination");
+
+      url.searchParams.set("page", page);
+
+      window.location.href = url.href;
+    });
+  });
+}
+
+// Checkbox Multi
+const checkboxMulti = document.querySelector("[checkbox-multi]")
+if(checkboxMulti){
+  const inputCheckAll = checkboxMulti.querySelector("input[name='checkall']");
+  const inputsId = checkboxMulti.querySelectorAll("input[name='id']");
+  inputCheckAll.addEventListener("click", () => {
+    if(inputCheckAll.checked){
+      inputsId.forEach(item => {
+        item.checked = true;
+      });
+    } else{
+      inputsId.forEach(item => {
+        item.checked = false;
+      });
+    }
+  })
+  inputsId.forEach(item => {
+    item.addEventListener("click", () => {
+      const countChecked = checkboxMulti.querySelectorAll("input[name='id']:checked").length;
+      if(countChecked == inputsId.length){
+        inputCheckAll.checked = true;
+      } else{
+        inputCheckAll.checked = false;
+      }
+    })
+  });
+}
+
+//Form change multi
+const formChangeMulti = document.querySelector("[form-change-multi]");
+if(formChangeMulti) {
+  formChangeMulti.addEventListener("submit", (e) => {
+    e.preventDefault();
+    const checkboxMulti = document.querySelector("[checkbox-multi]");
+    const inputsChecked = checkboxMulti.querySelectorAll("input[name='id']:checked");
+    const typeChange = e.target.elements.type.value;
+    if(typeChange == "delete-all") {
+      const isConfirm = confirm("Bạn có chắc muốn xóa những sản phẩm này?");
+
+      if(!isConfirm) {
+        return;
+      }
+    }
+    if(inputsChecked.length > 0){
+      let ids = [];
+      const inputIds = formChangeMulti.querySelector("input[name=ids]");
+      inputsChecked.forEach(item => {
+        const id = item.value;
+        if(typeChange == "change-position") {
+          const position = item.closest("tr").querySelector("input[name='position']").value;
+          ids.push(`${id}-${position}`)
+        } else {
+          ids.push(id)
+        }
+      })
+      inputIds.value = ids.join(", ");
+      formChangeMulti.submit();
+    } else{
+      alert("Vui long chon it nhat 1 ban ghi");
+    }
+  })
+}
+
+// Show Alert
+const showAlert = document.querySelector("[show-alert]");
+if(showAlert) {
+  const time = parseInt(showAlert.getAttribute("data-time"));
+  const closeAlert = showAlert.querySelector("[close-alert]");
+
+  setTimeout(() => {
+    showAlert.classList.add("alert-hidden");
+  }, time);
+
+  closeAlert.addEventListener("click", () => {
+    showAlert.classList.add("alert-hidden");
+  });
+}
+// End Show Alert
+
+// Upload Image
+const uploadImage = document.querySelector("[upload-image]");
+if(uploadImage) {
+  const uploadImageInput = document.querySelector("[upload-image-input]");
+  const uploadImagePreview = document.querySelector("[upload-image-preview]");
+
+  uploadImageInput.addEventListener("change", (e) => {
+    const file = e.target.files[0];
+    if(file) {
+      uploadImagePreview.src = URL.createObjectURL(file);
+    }
+  });
+}
+// End Upload Image
